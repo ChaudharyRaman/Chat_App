2fea37918100be8b38b220b5872f04201236db13
ChaudharyRaman <ramanchaudhary2704@gmail.com>
2023-01-04 23:09:11 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -57,7 +57,7 @@ const server = app.listen(PORT, () => {
 const io = require('socket.io')(server, {
     pingTimeout: 60000,
     cors: {
-        origin: "http://localhost:3000",
+        origin: ["http://localhost:3000","https://gregarious-pavlova-abe1e7.netlify.app/"],
     }
 })
 io.on('connection', (socket) => {
