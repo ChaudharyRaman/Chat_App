e8769e3cd633baf43e0b520bae5e35b72e2851f9
ChaudharyRaman <ramanchaudhary2704@gmail.com>
2023-05-07 21:28:10 +0530

    deploy

--- server/server.js
+++ server/server.js
@@ -30,7 +30,6 @@ app.use("/api/message", messageRoutes);
 // ----------------------------DEPLOYMENT--------------------------------------
 
 const __dirname1 = path.resolve();
-// console.log(__dirname1, '../client/build');
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "../client/build")));
   app.get("*", (req, res) => {
